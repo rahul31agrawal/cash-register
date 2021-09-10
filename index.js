aa0a9f28a9837031c86171342d0763e60415df36
rahul31agrawal <rahul31agrawal@gmail.com>
2021-09-10 19:15:56 +0530

    input-output-part-done

---
+++ index.js
@@ -0,0 +1,42 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+
+
+checkButton.addEventListener("click", function validateBillCashAmount() {
+    hideMessage();
+    if(billAmount.value > 0 ){
+
+        if(cashGiven.value >= billAmount.value){
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+
+            calculateChange(amountToBeReturned);
+
+        }else{
+            showMessage("The cash provided should be atleast be equal to the bill amount");
+
+        }
+
+    }else{
+        
+        showMessage("Invalid Bill Amount");
+    }
+
+
+
+});
+
+function calculateChange() {
+
+}
+
+function hideMessage() {
+    message.style.display = "none";
+}
+
+
+function  showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg ;
+}
